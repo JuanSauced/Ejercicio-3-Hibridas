b45526abc904a128796e833feb463c4037c6a305
bemazepedaib <bemazepedaib@ittepic.edu.mx>
2022-10-23 20:12:38 -0600

    Tareas Pendientes (Zepeda)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TasksService } from '../services/tasks.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public tasks: string[];
+  public task: string;
+  @ViewChild('inputText') input;
 
+  constructor(private taskService: TasksService) {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  addTask() {
+    this.taskService.addTask(this.task);
+    this.tasks = this.taskService.getTasks();
+    console.log(this.tasks);
+    this.task = "";
+    this.input.setFocus();
+  }
+
+  public removeTask(pos: number) {
+    this.taskService.removeTask(pos);
+    this.tasks = this.taskService.getTasks();
+    console.log(this.tasks);
+  }
+
+  public completeTask(pos: number) {
+    let taskAux = this.taskService.getTask(pos);
+    this.taskService.completeTask(taskAux);
+    this.taskService.removeTask(pos);
+    this.tasks = this.taskService.getTasks();
+    console.log(this.tasks);
+  }
 }
